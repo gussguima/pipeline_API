392e084507cc39a0d1a61e94edaf3af3cc5ca4e7
Gustavo Guimarães <gustavojobwork@gmail.com>
2025-07-17 13:16:21 -0300

    salvando

--- tests/extract.py
+++ tests/extract.py
@@ -1,28 +1,74 @@
 # %%
 import requests
-import time
+from sqlalchemy import create_engine, Column, String, Float, Integer, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+from time import sleep
+import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# configuração do banco de dados
+DATABASE_URL = os.getenv("DATABASE_KEY")
+
+# Criação da engine e da sessão
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+# Definição do modelo de dados
+class BitcoinDados(Base):
+    __tablename__ = "bitcoin_dados"
+    
+    id = Column(Integer, primary_key=True)
+    valor = Column(Float)
+    moeda = Column(String(10))
+    cotacao = Column(String(10))
+    timestamp = Column(DateTime)
+
+# Cria a tabela (se não existir)
+Base.metadata.create_all(engine)
 
 def extract():
-    url = "https://api.coinbase.com/v2/prices/spot"
-    response = requests.get(url)
-    return response.json()
+    """Extrai o JSON completo da API da Coinbase."""
+    url = 'https://api.coinbase.com/v2/prices/spot'
+    resposta = requests.get(url)
+    return resposta.json()
 
 def transform(dados_json):
-    valor = (dados_json['data']['amount'])
+    """Transforma os dados brutos da API e adiciona timestamp."""
+    valor = float(dados_json['data']['amount'])
     moeda = dados_json['data']['base']
-    cotação = dados_json['data']['currency']
-    dados_tratados = {
-        "valor": valor,
-        "moeda": moeda,
-        "cotação": cotação
-    }
-    
+    cotacao = dados_json['data']['currency']
+    dados_tratados = BitcoinDados(
+        valor=valor,
+        moeda=moeda,
+        cotacao=cotacao,
+        timestamp=datetime.now()
+    )
     return dados_tratados
+
+def saveData(dados):
+    """Salva os dados no PostgreSQL usando SQLAlchemy."""
+    with Session() as session:
+        session.add(dados)
+        session.commit()
+        print("Dados salvos no PostgreSQL!")
 
 # %%
 if __name__ == "__main__":
     while True:
+        # Extração e tratamento dos dados
         dados_json = extract()
         dados_tratados = transform(dados_json)
-        print(dados_tratados)
-        time.sleep(5)
+
+        # Mostrar os dados tratados
+        print("Dados Tratados:")
+
+        # Salvar no PostgreSQL
+        saveData(dados_tratados)
+
+        # Pausar por 15 segundos
+        print("Aguardando 15 segundos...")
+        sleep(15)
